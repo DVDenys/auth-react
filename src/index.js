cea21c64b88e4a518b58876119d15b67196ba70b
dvdenys <denysdymnych@gmail.com>
2023-05-04 12:33:25 +0300

    added deploy and readme

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./style/index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="auth-react">
       <Provider store={store}>
         <App />
       </Provider>
